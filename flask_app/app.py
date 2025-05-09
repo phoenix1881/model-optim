2d1d2ddf2ad0871beed03f0f67c76967e8d4dac0
Tejdeep Chippa <72910322+phoenix1881@users.noreply.github.com>
2025-05-09 18:04:30 -0400

    Update app.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -63,6 +63,7 @@ app = Flask(__name__)
 # === Model Config ===
 model_name = "TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T"
 checkpoint_path = "/mnt/object/artfacts/medical-qa-model/model.pth"
+#checkpoint_path = "/Users/tejdeepchippa/Desktop/All/model-optim/checkpoints/TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T/lit_model.pth"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # === Load tokenizer
@@ -70,9 +71,8 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 tokenizer.pad_token = tokenizer.eos_token
 
 # === Load model from checkpoint
-print("🔄 Loading TinyLLaMA via Lit-GPT...")
+print("Loading TinyLLaMA via Lit-GPT...")
 model = GPT.from_name(model_name)
-model = model.to_empty(device)
 state_dict = torch.load(checkpoint_path, map_location=device)
 model.load_state_dict(state_dict, strict=False)
 model.eval()
